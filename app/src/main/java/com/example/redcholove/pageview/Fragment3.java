4b9541476e2b8749288ea69b86097f74eb52e796
redcholove <aa840211aa@gmail.com>
2016-08-27 15:14:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/redcholove/pageview/Fragment3.java
@@ -0,0 +1,20 @@
+package com.example.redcholove.pageview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by redcholove on 2016/8/27.
+ */
+public class Fragment3 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frag3, container, false);
+    }
+}
